b251a1ba7979a486fd8124b7ec3bda6b5f0dd5d2
Raphael Fortunato <fortunatoraphael@hotmail.com>
2025-02-19 16:43:10 -0300

    Ajuste do Entity, para criar tabela, mas não deu certo

--- loja-nescoffee/src/main/java/br/com/lojanescoffee/LojaNescoffeeApplication.java
+++ loja-nescoffee/src/main/java/br/com/lojanescoffee/LojaNescoffeeApplication.java
@@ -2,7 +2,9 @@ package br.com.lojanescoffee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+//@EntityScan(basePackages = "br.com.lojanescoffee.model")
 @SpringBootApplication
 public class LojaNescoffeeApplication {
 
