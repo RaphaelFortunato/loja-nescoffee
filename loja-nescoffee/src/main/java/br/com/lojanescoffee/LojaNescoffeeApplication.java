e6732471f59c87de8b368b550f33f374c4072818
Raphael Fortunato <fortunatoraphael@hotmail.com>
2025-02-19 10:33:31 -0300

    Criação do projeto Loja Nescoffee

---
+++ loja-nescoffee/src/main/java/br/com/lojanescoffee/LojaNescoffeeApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojanescoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaNescoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaNescoffeeApplication.class, args);
+	}
+
+}
---
+++ loja-nescoffee/src/test/java/br/com/lojanescoffee/LojaNescoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojanescoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaNescoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
