c10b7cde1b581076afc566ca40848c426bf4079a
Raphael Fortunato <fortunatoraphael@hotmail.com>
2025-02-20 17:31:33 -0300

    Atualização do pom.xml e do entityscan

--- loja-nescoffee/src/main/java/br/com/lojanescoffee/LojaNescoffeeApplication.java
+++ loja-nescoffee/src/main/java/br/com/lojanescoffee/LojaNescoffeeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-//@EntityScan(basePackages = "br.com.lojanescoffee.model")
+@EntityScan(basePackages = "br.com.lojanescoffee.model")
 @SpringBootApplication
 public class LojaNescoffeeApplication {
 
